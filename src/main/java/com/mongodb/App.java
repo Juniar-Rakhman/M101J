1e9a9769d53fd7645feb832cb35c73e688dfc53d
juniar.rakhman <juniar.rahman@gmail.com>
2015-06-08 23:14:13 +0800

    homework 2 in progress

--- src/main/java/com/mongodb/App.java
+++ src/main/java/com/mongodb/App.java
@@ -2,17 +2,34 @@ package com.mongodb;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.bson.Document;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
-        MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(100).build();
-        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017), options);
 
-        MongoDatabase db = client.getDatabase("test").withReadPreference(ReadPreference.secondary());
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(App.class, "/");
 
-        MongoCollection coll = db.getCollection("test");
+        MongoClient client = new MongoClient();
 
+        MongoDatabase database = client.getDatabase("students");
+        MongoCollection<Document> collection = database.getCollection("grades");
 
-
+        List<Document> all = collection.find().into(new ArrayList<Document>());
+        for (Document cursor : all) {
+            System.out.println(cursor);
+        }
     }
 }
