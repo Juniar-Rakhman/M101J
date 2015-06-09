3feee9e40ad278f2e4c3571ab7aea02b085f4f0c
juniar.rakhman <juniar.rahman@gmail.com>
2015-06-09 22:25:05 +0800

    homework 2 completed

--- src/main/java/com/mongodb/App.java
+++ src/main/java/com/mongodb/App.java
@@ -4,12 +4,10 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import freemarker.template.Configuration;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.mongodb.client.model.Filters.and;
-import static com.mongodb.client.model.Filters.eq;
 
 public class App {
     public static void main(String[] args) {
@@ -22,16 +20,24 @@ public class App {
         MongoDatabase database = client.getDatabase("students");
         MongoCollection<Document> collection = database.getCollection("grades");
 
-//        myDoc = collection.find(exists("i")).sort(descending("i")).first();
-//        System.out.println(myDoc.toJson());
+        Bson filter = new Document("type", "homework");
+        Bson sort = new Document("student_id", 1).append("score", 1);
 
-        List<Document> all = collection.find(eq("type", "homework")).into(new ArrayList<Document>());
+        List<Document> all = collection
+                .find(filter)
+                .sort(sort)
+                .into(new ArrayList<Document>());
+
         int prevStudent = -1;
+
         for (Document document : all) {
             if (prevStudent != document.getInteger("student_id")) {
+                System.out.println(document + "deleted");
                 prevStudent = document.getInteger("student_id");
+                collection.deleteOne(new Document("student_id", document.getInteger("student_id"))
+                        .append("score", document.getDouble("score")));
             } else {
-                List<Document> scores = collection.find(and(eq("student_id", document.getInteger("student_id")), eq("type", "homework"))).into(new ArrayList<Document>());
+                System.out.println(document);
             }
         }
 
